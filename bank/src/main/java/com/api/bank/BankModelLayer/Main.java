2f46805438403990421813e71470cd3c974fbd9f
Tarcio Diniz <tarciodiniz0@gmail.com>
2023-11-06 11:28:02 -0300

    Create Main.java

---
+++ bank/src/main/java/com/api/bank/BankModelLayer/Main.java
@@ -0,0 +1,14 @@
+package com.api.bank.BankModelLayer;
+
+import com.api.bank.BankModelLayer.Application.Account;
+import com.api.bank.BankModelLayer.Domain.Account.CurrentAccount;
+import com.api.bank.BankModelLayer.Domain.Account.SavingsAccount;
+
+public class Main {
+    public static void main(String[] args) {
+        Account contaCorrente = new CurrentAccount();
+        Account contaPoupanca = new SavingsAccount();
+
+        contaPoupanca.
+    }
+}
