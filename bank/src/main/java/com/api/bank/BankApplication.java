5bd661f4b1542b9339a86218e2fd9fff37002b1e
Tarcio Diniz <tarciodiniz0@gmail.com>
2023-11-28 16:55:22 -0300

    chore(logging): add logger to BankApplication class
    - Integrate logging in the BankApplication class using Log4j2.
    - Set up log file configuration using LogConfig.
    - Add an informational log message on application startup.

--- bank/src/main/java/com/api/bank/BankApplication.java
+++ bank/src/main/java/com/api/bank/BankApplication.java
@@ -1,13 +1,22 @@
 package com.api.bank;
 
+import com.api.bank.configuration.LogConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class BankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        LogConfig.setLogFile("BankApplication");
+
+        SpringApplication.run(BankApplication.class, args);
+
+        Logger logger = LogManager.getLogger(BankApplication.class);
+        logger.info("Iniciando Aplicação");
+
+    }
 
 }
